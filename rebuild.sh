6caa755bb6d94a4e3eb33aac6fd68a2e02cfb2b3
Jeffrey McBeth <mcbeth@broggs.org>
2020-05-14 15:51:28 -0400

    trying to make the tools directory independent

--- rebuild.sh
+++ rebuild.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-rm geojson/* && ruby scripts/make_geojson.rb && geojson-merge geojson/* > features2.geojson
+rm geojson/* && scripts/make_geojson.rb && geojson-merge geojson/* > features2.geojson
